c4826199dc58dd799bc35425949eb384391136aa
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-13 09:52:38 +0300

    Final Commit

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+num1 = 20
+num2 = 30
+addition = num1 + num2
+subtraction = num2 - num1
+product = num1 * num2
+print("Addition of", num1 ," and ", num2," is ",addition) 
+print("Subtraction of", num1 ," and ", num2," is ",subtraction) 
+print("Product of", num1 ," and ", num2," is ",product)
