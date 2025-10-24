861355fdac2cea5f033478901923775c25fc15b4
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-24 08:28:58 +0300

    Test

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,11 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            return num1 / num2
-        case _:
-            return "Invalid entry!!!"     
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2  
+    elif operation == "multiply":
+        return num1 * num2  
+    elif operation == "divide":
+        return num1 / num2
+    else:
+        return "Invalid entry"
