b5e8a1a151c52950e00687cc9c20cd1bb7cf696f
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-24 08:18:47 +0300

    Trial

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+def perform_operation():
+    match operation:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case _:
+            return "Invalid entry!!!"     
