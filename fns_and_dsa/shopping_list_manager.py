a0b4833a742a1b77d42491c6644494cb64b373c9
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-25 08:19:01 +0300

    Final

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,26 @@
+def display():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display()
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            item_add = input("Add an item: ")
+            shopping_list.append(item_add)
+        elif choice == '2':
+            item_remove = input("Which item do you want to remove? ")
+            shopping_list.append(item_remove)
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice")
+if __name__ == "__main__":
+    main()
