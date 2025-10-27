6aeecf20c384813a260b2924293b355d1ba9bbf8
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-27 09:28:58 +0300

    I have already tested the code and it worked perfectly!!

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = account_balance
+    def deposit(self,amount):
+        self.account_balance += amount
+    def withdraw(self,amount):
+        if amount <= 0:
+            print("Withdrawal amount must be positive.")
+            return False
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False           
+    def display_balance(self):
+        print(f"Your current bank account balance is {self.account_balance}") 
