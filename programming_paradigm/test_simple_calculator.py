22b600efc852de37c626df86c6d7f934bb243484
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-10-28 09:33:28 +0300

    I added more test cases and rectified the namings of the methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -3,15 +3,19 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 2), 4)
-    def test_subtract(self):
+        self.assertEqual(self.calc.add(-2, 2), 0)
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2, 2), 0)
-    def test_multiply(self):
+        self.assertEqual(self.calc.subtract(2, -2), 4)
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 2), 4)
-    def test_divide(self):
+        self.assertEqual(self.calc.multiply(-2, 2), -4)
+    def test_division(self):
         self.assertEqual(self.calc.divide(2, 2), 1)
-    def test_divide_byzero(self):
+        self.assertEqual(self.calc.divide(2, -2), -1)
+    def test_division_byzero(self):
         self.assertEqual(self.calc.divide(2, 0), None)
 if __name__ == "__main__":
     unittest.main()
