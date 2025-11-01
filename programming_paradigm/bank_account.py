7526dd0dd572cd17e06e8c4d661f0c7afa8455da
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-01 23:29:11 +0300

    Bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,22 @@
 class BankAccount:
+    
     def __init__(self,account_balance):
         self.account_balance = account_balance
+        
     def deposit(self,amount):
         self.account_balance += amount
+        
     def withdraw(self,amount):
         if amount <= 0:
             print("Withdrawal amount must be positive.")
             return False
+         
         if self.account_balance >= amount:
             self.account_balance -= amount
             return True
+        
         else:
-            return False           
+            return False   
+                
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}") 
