7713bcf0afc5c86d3c16554feefe3f3ec9fae63a
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-13 11:30:54 +0300

    Added the __str__ method

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,12 +2,18 @@ class Book:
     def __init__(self,title,author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 class EBook(Book):
     def __init__(self,title,author,file_size ):
         self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, Size: {self.file_size}KB"
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
         self.page_count = page_count
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 class Library:
     books = [Book, EBook, PrintBook]
     def add_book(self, book):
