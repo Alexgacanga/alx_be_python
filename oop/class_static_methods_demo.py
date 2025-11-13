47a524d79d54879498ae1db94b8b16795632f36f
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-13 12:59:51 +0300

    Indentation error

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -3,7 +3,7 @@ class Calculator:
     def add(a, b):
         return a + b
     @classmethod
-    def multiply(cls,a,b):
+    def multiply(cls, a, b):
         calculation_type = "Arithmetic Operations"
         print(f"Calculation type: {calculation_type}")
         return a * b
