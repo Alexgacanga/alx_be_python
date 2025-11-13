9e5c8646e789e2a2e4a8f28ca2fca0dd706d3fad
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-11-13 11:21:45 +0300

    Library System

---
+++ oop/library_system.py
@@ -0,0 +1,17 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+class Ebook(Book):
+    def __init__(self,title,author,file_size ):
+        self.file_size = file_size
+class Printbook(Book):
+    def __init__(self,title,author,page_count):
+        self.page_count = page_count
+class Library:
+    books = [Book, EBook, PrintBook]
+    def add_book(self, book):
+        for b in self.books:
+            self.books.append(book)
+    def list_books(self):
+        print(self.books)
